aef076a149c00f28773ee10427becb28b58d7aba
Bhanu <42rapardskull42@gmail.com>
2025-04-14 09:35:55 +0000

    Comparisions

---
+++ 01-basics/04-comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2  >= 1);
+// console.log(2 < 1 );
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// **************** avoid these type of comparisions ************** //
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // comparsions convert null to a number, treating it as 0 that's why its true and null > 0 is false //
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
