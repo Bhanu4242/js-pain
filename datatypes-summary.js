011e44828aa49c317b1d9ea8283e9b5be063078d
Bhanu <42rapardskull42@gmail.com>
2025-04-14 10:39:46 +0000

    Datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3414424321289729872n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Pain",
+    age:  22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");    
+}
+
+console.log(typeof id);
