28696b8ca49290b244e60a608f00d2075e29aa17
Bhanu <42rapardskull42@gmail.com>
2025-04-16 11:44:29 +0000

    json talk

--- 02-basics/04-objects.js
+++ 02-basics/04-objects.js
@@ -46,10 +46,36 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedI'));
+// console.log(tinderUser.hasOwnProperty('isLoggedI'));
+
+
+const course = {
+    coursename: "odin projects",
+    price: "999",
+    courseInstructor: "pain"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "pain",
+//     "coursename": "odin projects",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
