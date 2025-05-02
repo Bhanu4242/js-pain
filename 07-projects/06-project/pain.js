c19c36168781f8583ba8408f8f67a2880f96189b
Bhanu <42rapardskull42@gmail.com>
2025-05-02 21:29:27 +0000

    2 projects and settimeout

---
+++ 07-projects/06-project/pain.js
@@ -0,0 +1,31 @@
+// generate a random color
+
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  let intervalid;
+  
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+  
+  const startChangingColor = function () {
+    if (!intervalid) {
+      intervalid = setInterval(changeBgColor, 1000);
+    }
+  };
+  
+  const stopChangingColor = function () {
+    clearInterval(intervalid);
+    intervalid = null;
+  };
+  
+  document.querySelector('#start').addEventListener('click', startChangingColor);
+  
+  document.querySelector('#stop').addEventListener('click', stopChangingColor);
